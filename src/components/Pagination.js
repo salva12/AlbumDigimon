e23790ec5b07c777592437e1b86be788399d3740
salva <salvamanu12@gmail.com>
2024-09-07 19:46:39 -0300

    feat: :zap: Adding a paginator component.

---
+++ src/components/Pagination.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const Pagination = ({ currentPage, totalPages, onPageChange }) => {
+  const getPages = () => {
+    let pages = [];
+    for (let i = 1; i <= totalPages; i++) {
+      pages.push(i);
+    }
+    return pages;
+  };
+
+  return (
+    <nav aria-label="Page navigation">
+      <ul className="pagination">
+        <li className={`page-item ${currentPage === 1 ? 'disabled' : ''}`}>
+          <button className="page-link" onClick={() => onPageChange(currentPage - 1)} disabled={currentPage === 1}>
+            Previous
+          </button>
+        </li>
+        {getPages().map(page => (
+          <li key={page} className={`page-item ${currentPage === page ? 'active' : ''}`}>
+            <button className="page-link" onClick={() => onPageChange(page)}>
+              {page}
+            </button>
+          </li>
+        ))}
+        <li className={`page-item ${currentPage === totalPages ? 'disabled' : ''}`}>
+          <button className="page-link" onClick={() => onPageChange(currentPage + 1)} disabled={currentPage === totalPages}>
+            Next
+          </button>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Pagination;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,7 @@ import Footer from '../components/Footer'
 import Header from '../components/Header'
 import Album from '../components/Album'
 import Introduccion from '../components/Introduccion'
+import Pagination from '../components/Pagination';
 
 
 class Home extends React.Component{
@@ -12,19 +13,55 @@ class Home extends React.Component{
         error: null,
         data:{
             personajes:[{}],
-        }
-        
+        },
+        currentPage:1,
+        totalPages:5 
     };
     
     componentDidMount(){
-        this.getPersonajes();
+        this.getPersonajes(1);
     }
-    getPersonajes = async() => {
+
+  
+    handlePageChange = (page) => {
+        console.log('cambie de pagina')
+        this.setState({currentPage : page})
+      // Perform other actions like fetching data for the new page
+        this.getPersonajes(page);
+    };
+
+    getPersonajes = async(page) => {
         this.setState({loading:true,error:null});
+        let inf=0;
+        let sup=0;
+        switch (page) {
+            case 1:
+                inf=0
+                sup=9
+                break;
+            case 2:
+                inf=10
+                sup=19
+                break;
+            case 3:
+                inf=20
+                sup=29
+                break;
+            case 4:
+                inf=30
+                sup=39
+                break;
+            case 5:
+                inf=40
+                sup=49
+                break;
+            default:
+                break;
+        }
         try{
             const response = await fetch('https://digimon-api.vercel.app/api/digimon');
             const data = await response.json();
-            const datarecortada = data.slice(0,12);
+            const datarecortada = data.slice(inf,sup);
             console.log(datarecortada); 
             this.setState({
                 loading : false,
@@ -43,7 +80,7 @@ class Home extends React.Component{
         return (
             <React.Fragment>
             <Header />
-                <Introduccion titulo="Recordando Digimon" descripcion="Algunos de los personajes mas queridos e importantes de esta serie y juego mundialmente conocido."/>
+                <Introduccion titulo="Recordando Digimon" descripcion="Algunos de los personajes más icónicos de esta serie y juego mundialmente reconocido."/>
                 <div class="album py-5 bg-light">
                         <div class="container">                              
                                 <Album personajes={this.state.data.personajes}/>
@@ -51,6 +88,8 @@ class Home extends React.Component{
                             
                         </div>
                 </div>
+                <Pagination currentPage={this.state.currentPage} totalPages={this.state.totalPages} onPageChange={this.handlePageChange} />
+
             <Footer />
         </React.Fragment> 
         );
