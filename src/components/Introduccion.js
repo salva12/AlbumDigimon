9a550bae97cbb6ad1f33e152188fc46482686ed1
salva <salvamanu12@gmail.com>
2024-09-12 18:43:22 -0300

    feat: :zap: Introduction style enhanced

--- src/components/Introduccion.js
+++ src/components/Introduccion.js
@@ -1,10 +1,10 @@
 import {Component} from 'react';
-
+import './Introduccion.css';
 
 class Introduccion extends Component{
     render() {
         return (
-          <section class="jumbotron text-center">
+          <section class="jumbotrom-custom text-center">
               <div class="container">
                 <h1 className="title">{this.props.titulo}</h1>
                 <p class="lead text-muted">{this.props.descripcion}</p>
