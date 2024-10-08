146746bc2b632af58b9b6b70864fb5b2e2028305
salva <salvamanu12@gmail.com>
2024-10-08 20:35:37 -0300

    feat: :sparkles: Re-styling of introduction.

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,11 @@ import {Component} from 'react';
 
 
 class Header extends Component{
+
     render() {
+        const navbarCustom = {
+          background: '#eead18',
+        }
         return (
             <header>
             <div class="collapse bg-dark" id="navbarHeader">
@@ -23,7 +27,7 @@ class Header extends Component{
                 </div>
               </div>
             </div>
-            <div class="navbar navbar-dark bg-dark shadow-sm">
+            <div class="navbar navbar-dark shadow-sm" style={navbarCustom}>
               <div class="container d-flex justify-content-between">
                 <a href="/#" class="navbar-brand d-flex align-items-center">
                   <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" aria-hidden="true" class="mr-2" viewBox="0 0 24 24" focusable="false"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"/><circle cx="12" cy="13" r="4"/></svg>
--- src/components/Introduccion.js
+++ src/components/Introduccion.js
@@ -2,12 +2,32 @@ import {Component} from 'react';
 import './Introduccion.css';
 
 class Introduccion extends Component{
+
     render() {
+        const customBackground= {
+          background:'#3b9ff9',
+        }
+
+        const customTitle= {
+          color:'#f9b74e',
+          fontWeight:'bold',
+          fontFamily:'Georgia',
+          textShadow: '0px 2px 4px black'
+        }
+
+        const customDesc= {
+          color:'#f9b74e',
+          fontWeight:'bold',
+          fontSize:'1rem',
+          fontFamily:'Georgia',
+          textShadow: '0px 2px 4px black'
+        }
+
         return (
-          <section class="jumbotrom-custom text-center">
+          <section class="jumbotrom-custom text-center" style={customBackground}>
               <div class="container">
-                <h1 className="title">{this.props.titulo}</h1>
-                <p class="lead text-muted">{this.props.descripcion}</p>
+                <h1 className="title" style={customTitle}>{this.props.titulo}</h1>
+                <p class="lead" style={customDesc}>{this.props.descripcion}</p>
               </div>
             </section>
         );
